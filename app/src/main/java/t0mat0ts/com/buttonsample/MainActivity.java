c0dc77a300ddd7a27e3c2d8a419cba724f33ed86
佐々木知洋 <sasakitomohiro@sasakitomohironoMacBook-puro.local>
2018-06-22 21:36:27 +0900

    create android project

---
+++ app/src/main/java/t0mat0ts/com/buttonsample/MainActivity.java
@@ -0,0 +1,13 @@
+package t0mat0ts.com.buttonsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
