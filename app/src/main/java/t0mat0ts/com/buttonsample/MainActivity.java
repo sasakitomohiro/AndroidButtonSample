c6d29876c06c603b3dfd4b959ef11a0872a5aeab
佐々木知洋 <sasakitomohiro@sasakitomohironoMacBook-puro.local>
2018-06-22 21:54:04 +0900

    create basic button listener

--- app/src/main/java/t0mat0ts/com/buttonsample/MainActivity.java
+++ app/src/main/java/t0mat0ts/com/buttonsample/MainActivity.java
@@ -2,6 +2,7 @@ package t0mat0ts.com.buttonsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("button", "clicked");
+            }
+        });
     }
 }
