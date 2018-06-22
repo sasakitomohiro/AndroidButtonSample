4bf7db07dd4faf9a29be34cb68a81be5b50324d8
佐々木知洋 <sasakitomohiro@sasakitomohironoMacBook-puro.local>
2018-06-22 23:42:42 +0900

    use data binding

--- app/src/main/java/t0mat0ts/com/buttonsample/MainActivity.java
+++ app/src/main/java/t0mat0ts/com/buttonsample/MainActivity.java
@@ -1,19 +1,28 @@
 package t0mat0ts.com.buttonsample;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import t0mat0ts.com.buttonsample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+//        implements View.OnClickListener
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(this);
+//        setContentView(R.layout.activity_main);
+
+        // data binding
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setActivityMain(this);
+
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(this);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -22,12 +31,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        });
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                Log.e("button", "clicked");
-                break;
-        }
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.button:
+//                Log.e("button", "clicked");
+//                break;
+//        }
+//    }
+
+    public void button() {
+        Log.e("button", "clicked");
     }
 }
